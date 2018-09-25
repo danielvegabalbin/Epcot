0b5a7a4055506f627b8f5477c5b9c811c33a44ab
danielvegabalbin <daniel-vegabalbin@hotmail.com>
2018-09-25 01:02:31 -0500

    Add files via upload

---
+++ Juego/Bala.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour {
+    
+    private GameObject objetivo;
+    private float velocidad = 2;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direccion;
+        if (objetivo != null)
+        {
+            direccion = objetivo.transform.position - this.transform.position;
+            this.transform.position += velocidad * direccion * Time.deltaTime;
+
+        }
+
+    }
+    public void ActivarBala(Torre torre)
+    {
+        objetivo = torre.enemigo;
+    }
+}
---
+++ Juego/PoolingdeUnidades.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingdeUnidades : MonoBehaviour {
+    public static ArrayList unidades = new ArrayList();
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject unidad = GameObject.Find("Unidad_01");
+        GameObject temp;
+        Vector3 incremento = new Vector3(0, 1);
+        Vector3 posicion_actual = unidad.transform.position;
+        for (int i = 0; i < 10; i++)
+        {
+            temp = (GameObject)Instantiate(unidad, posicion_actual + incremento, Quaternion.identity);
+            posicion_actual = temp.transform.position;
+            unidades.Add(temp);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
